9ae20e17d18012b54761f414c8560bc2e654b792
diyanamendoza <diyana.amp@pm.me>
2021-08-14 11:50:20 -0700

    Add Adam Driver content

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-// import functions and grab DOM elements
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
+const horseSound = document.getElementById('horse-sound');
 
-// initialize state
+const dogImage = document.getElementById('dog-pic');
+const catImage = document.getElementById('cat-pic');
+const horseImage = document.getElementById('horse-pic');
+
+// const animalHeard = document.getElementById('animal-heard');
+
+// refactoring
+
+function playSoundChangeText (sound, animalName) {
+  sound.play();
+  // animalHeard.textContent = animalName;
+  // setTimeout(() => {
+  //   animalHeard.textContent = "";
+  // }, 3000);
+}
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+dogImage.addEventListener('click', () => {
+  playSoundChangeText (dogSound, "Dog");
+})
+
+catImage.addEventListener('click', () => {
+  playSoundChangeText (catSound, "Cat");
+})
+
+horseImage.addEventListener('click', () => {
+  playSoundChangeText (horseSound, "Horse");
+})
